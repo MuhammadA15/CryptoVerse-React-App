7441d1132c012754783670c662daeba4f49e9792
Muhammad Azam <haseebazam@live.com>
2021-11-14 02:30:58 -0600

    Update CryptoDetails.jsx

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -23,7 +23,7 @@ const CryptoDetails = () => {
 
     if(isFetching) return <Loader />
 
-    const time = ['3h','24h','7d','30d','3m','1y','3y','5y']
+    const time = ['24h','7d','30d','1y','5y']
 
     const stats = [
         { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price, { precision: 2 })}`, icon: <DollarCircleOutlined />},
