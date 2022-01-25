bd3fbe5343108e83221f5c63c2c24f73686278fa
Muhammad Azam <haseebazam@live.com>
2022-01-25 04:46:44 -0500

    Add files via upload

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-access-token': process.env.REACT_APP_COIN_KEY
+}
+
+const baseUrl = 'https://api.coinranking.com/v2';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`) 
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`)
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timePeriod }) => createRequest(`coin/${coinId}/history?timeperiod=${timePeriod}`)
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges')
+        }),
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery,
+    useGetCryptoHistoryQuery,
+    useGetExchangesQuery,
+} = cryptoApi;
